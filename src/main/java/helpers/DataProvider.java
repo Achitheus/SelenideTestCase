f412ba1557f3b4c9af27df2abe8a26f3eb838ff2
Yuri <yur_yurch@mail.ru>
2023-10-10 21:53:01 +0300

    DataProvider contain actual data now

--- src/main/java/helpers/DataProvider.java
+++ src/main/java/helpers/DataProvider.java
@@ -2,6 +2,7 @@ package helpers;
 
 import org.junit.jupiter.params.provider.Arguments;
 
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
@@ -17,20 +18,28 @@ public class DataProvider {
     public static Stream<Arguments> checkSearchResultsByEnumFilter() {
         String url = "https://ya.ru/";
         String serviceTitle = "Маркет";
+        String section = "Электроника";
+        String category = "Смартфоны";
+        Map<String, Set<String>> enumFilters1 = Map.of("Производитель", Set.of("ASUS"));
+        Map<String, Set<String>> enumFilters2 = Map.of("Производитель", Set.of("Black Shark"));
+        Map<String, Set<String>> enumFilters3 = Map.of("Производитель", Set.of("OnePlus"));
+        Map<String, Set<String>> enumFilters4 = Map.of("Производитель", Set.of("Apple"));
+        Map<String, Set<String>> enumFilters5 = Map.of("Производитель", Set.of("Google"));
+        Map<String, Set<String>> enumFilters6 = Map.of("Производитель", Set.of("Seals"));
 
-        String section1 = "Электроника";
-        String category1 = "Смартфоны";
-        Map<String, Set<String>> enumFilters1 = new HashMap<>();
-        enumFilters1.put("Производитель", Set.of("Apple"));
-
-        String section2 = "Ноутбуки и компьютеры";
-        String category2 = "Ноутбуки";
-        Map<String, Set<String>> enumFilters2 = new HashMap<>();
-        enumFilters2.put("Производитель", Set.of("Samsung", "Ark", "ASUS", "BlackBerry"));
-
+        Map<String, Set<String>> enumCheckSets1 = Map.of("Производитель", enumFilters1.get("Производитель"));
+        Map<String, Set<String>> enumCheckSets2 = Map.of("Производитель", enumFilters2.get("Производитель"));
+        Map<String, Set<String>> enumCheckSets3 = Map.of("Производитель", Set.of("OnePlus", "One Plus"));
+        Map<String, Set<String>> enumCheckSets4 = Map.of("Производитель", Set.of("Apple", "IPhone"));
+        Map<String, Set<String>> enumCheckSets5 = Map.of("Производитель", enumFilters5.get("Производитель"));
+        Map<String, Set<String>> enumCheckSets6 = Map.of("Производитель", enumFilters6.get("Производитель"));
         return Stream.of(
-                Arguments.of(url, serviceTitle, section1, category1, enumFilters1),
-                Arguments.of(url, serviceTitle, section2, category2, enumFilters2)
+                Arguments.of(url, serviceTitle, section, category, enumFilters1, enumCheckSets1),
+                Arguments.of(url, serviceTitle, section, category, enumFilters2, enumCheckSets2),
+                Arguments.of(url, serviceTitle, section, category, enumFilters3, enumCheckSets3),
+                Arguments.of(url, serviceTitle, section, category, enumFilters4, enumCheckSets4),
+                Arguments.of(url, serviceTitle, section, category, enumFilters5, enumCheckSets5),
+                Arguments.of(url, serviceTitle, section, category, enumFilters6, enumCheckSets6)
         );
     }
 }
--- src/test/java/ru/bellintegrator/ru/yandex/market/MarketTest.java
+++ src/test/java/ru/bellintegrator/ru/yandex/market/MarketTest.java
@@ -2,11 +2,9 @@ package ru.bellintegrator.ru.yandex.market;
 
 import com.codeborne.selenide.ElementsCollection;
 import com.codeborne.selenide.SelenideElement;
-import com.codeborne.selenide.junit5.ScreenShooterExtension;
 import io.qameta.allure.Feature;
 import io.qameta.allure.Step;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.extension.RegisterExtension;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.slf4j.Logger;
@@ -30,8 +28,6 @@ import static helpers.Assertions.selenideAssertWithStep;
 
 public class MarketTest extends BaseTest {
     public static final Logger log = LoggerFactory.getLogger(MarketTest.class);
-    @RegisterExtension
-    static ScreenShooterExtension screenshotEmAll = new ScreenShooterExtension(true);
 
     /**
      * Тест-кейс: <p>
@@ -46,11 +42,12 @@ public class MarketTest extends BaseTest {
      * Тест должен работать для любого производителя из списка:
      * ASUS, Black Shark, OnePlus, Google, Seals.
      *
-     * @param url         Начальная страница с сервисами, на которую следует перейти.
-     * @param service     Сервис, в который следует перейти.
-     * @param section     Секция, внутри которой содержится нужная {@code category}.
-     * @param category    Категория товаров, в которую следует перейти.
-     * @param enumFilters Фильтры перечислений в формате Map<Название фильтра, набор названий чекбоксов>.
+     * @param url           Начальная страница с сервисами, на которую следует перейти.
+     * @param service       Сервис, в который следует перейти.
+     * @param section       Секция, внутри которой содержится нужная {@code category}.
+     * @param category      Категория товаров, в которую следует перейти.
+     * @param enumFilters   Фильтры перечислений в формате Map<Название фильтра, набор названий чекбоксов>.
+     * @param enumCheckSets Наборы проверочных слов для проверки соответствия товаров установленным фильтрам {@code enumFilters}.
      * @author Юрий Юрченко
      */
     @Feature("Поиск товаров")
@@ -58,37 +55,37 @@ public class MarketTest extends BaseTest {
     @ParameterizedTest(name = "{displayName} : {arguments}")
     @MethodSource(value = "helpers.DataProvider#checkSearchResultsByEnumFilter")
     public void marketTest(String url, String service, String section, String category,
-                           Map<String, Set<String>> enumFilters) {
+                           Map<String, Set<String>> enumFilters, Map<String, Set<String>> enumCheckSets) {
         CategoryGoods categoryGoods =
                 open(url, YandexMain.class)
                         .goToService(service, MarketMain.class)
                         .toSectionCategory(section, category)
                         .setEnumFilters(enumFilters, CheckboxProcessMode.MARK);
 
-        marketCheckAllPages(categoryGoods, enumFilters);
+        marketCheckAllPages(categoryGoods, enumCheckSets);
     }
 
     /**
-     * Проходит по всем страницам, проверяя каждую на соответствие товаров установленным
-     * фильтрам поиска {@code enumFilters}.
+     * Проходит по всем страницам, проверяя каждую на соответствие товаров (не чувствительно
+     * к регистру) наборам проверочных слов {@code enumCheckSets}.
      *
      * @param categoryGoods Пэйдж обджект страницы (с пагинацией) товаров, подлежащих проверке.
-     * @param enumFilters   Фильтры перечислений в формате Map<Название фильтра, набор названий чекбоксов>,
-     *                      на соответствие которым производится проверка товаров.
+     * @param enumCheckSets Наборы для проверки соответствия установленным фильтрам перечислений в
+     *                      формате Map<Название фильтра, набор проверочных слов>.
      * @author Юрий Юрченко
      */
-    // TODO возможно стоит этот метод обобщить, сделать универсальным и поместить в ПО
+    // TODO превратить метод во что-то реюзабельное и вменяемое
     @Step("Проверка товаров на всех страницах")
-    public static void marketCheckAllPages(CategoryGoods categoryGoods, Map<String, Set<String>> enumFilters) {
+    public static void marketCheckAllPages(CategoryGoods categoryGoods, Map<String, Set<String>> enumCheckSets) {
         int infinityCyclePreventer = 0;
         do {
             infinityCyclePreventer++;
-            log.debug("Страница {}", infinityCyclePreventer);
             ElementsCollection productNameEls = categoryGoods.getPageProductNames();
+            log.debug("Страница {}. Число товаров: {}", infinityCyclePreventer, productNameEls.size());
             log.trace("Названия товаров на {} странице: {}", infinityCyclePreventer, productNameEls.texts());
 
             SelenideElement badName = productNameEls.find(not(match("",
-                    nameEl -> enumFilters.get("Производитель").stream().anyMatch(
+                    nameEl -> enumCheckSets.get("Производитель").stream().anyMatch(
                             brand -> nameEl.getText().toLowerCase().contains(brand.toLowerCase())))));
             boolean badNameExists = badName.exists();
             // Не забыть вернуть значение STRICT!!!
@@ -96,10 +93,10 @@ public class MarketTest extends BaseTest {
             selenideAssertWithStep(badName,
                     (el, message) -> badNameExists ? el.shouldNot(exist.because(message), Duration.ZERO) : el,
                     badNameExists
-                            ? "<<< FAIL >>> На стр. " + infinityCyclePreventer + " наименование товара \""
-                            + badName.getText() + "\" не соответствует фильтру \"Производитель\": " + enumFilters.get("Производитель")
+                            ? "<<<<<< FAIL >>>>>> На стр. " + infinityCyclePreventer + " наименование товара \""
+                            + badName.getText() + "\" не соответствует фильтру \"Производитель\": " + enumCheckSets.get("Производитель")
                             : "На стр. " + infinityCyclePreventer + " все названия товаров соответствуют фильтру \"Производитель\": "
-                            + enumFilters.get("Производитель"));
+                            + enumCheckSets.get("Производитель"));
             assertionMode = STRICT;
         } while (categoryGoods.nextPage() && infinityCyclePreventer < 1000);
         log.info("Обработано {} страниц товаров", infinityCyclePreventer);
